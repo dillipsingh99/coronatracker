0e6548d3cbb0caba8c37edd0275ae1018f949165
dillipsingh <dk381759@gmail.com>
2021-09-13 19:20:42 +0545

    Adding alloweded host to setting

--- covid19/settings.py
+++ covid19/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-v35hl40l4#ry@e$&zzqn!%lkb$wbnb6q9lr-&-^(1_=wl*sqro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# 'covid19wwtracker.herokuapp.com'
+ALLOWED_HOSTS = ['covid19wwtracker.herokuapp.com']
+
 # 'covid19wwtracker.herokuapp.com'
 # Application definition
 
