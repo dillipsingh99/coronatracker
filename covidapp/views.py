de544a62c63c03c078ae07603bc11471dc9a7a6e
dillipsingh <dk381759@gmail.com>
2021-09-13 19:11:49 +0545

    New update

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 import json
 import requests
+from django.shortcuts import render
 
 url = "https://covid-193.p.rapidapi.com/statistics"
 
@@ -11,11 +11,6 @@ headers = {
 
 response = requests.request("GET", url, headers=headers).json()
 
-# print(response.text)
-# print(response.text)
-
-
-
 def home(request):
     mylist = []
     noofresults = int(response['results'])
@@ -23,17 +18,35 @@ def home(request):
         mylist.append(response['response'][x]['country'])
     if request.method=='POST':
         selectedcountry = request.POST['selectedcountry']
-        # print(selectedcountry)
         noofresults = int(response['results'])
         for x in range(0, noofresults):
             if selectedcountry==response['response'][x]['country']:
-                new = response['response'][x]['cases']['new']
-                active = response['response'][x]['cases']['active']
-                critical = response['response'][x]['cases']['critical']
-                recovered = response['response'][x]['cases']['recovered']
-                total = response['response'][x]['cases']['total']
-                death = int(total)-int(active)-int(recovered)
-        context = {'selectedcountry':selectedcountry, 'mylist': mylist, 'new':new, 'active':active, 'critical':critical, 'recovered':recovered, 'total':total, 'death':death }
+                population = response['response'][x]['population']
+                new_cases = response['response'][x]['cases']['new']
+                active_cases = response['response'][x]['cases']['active']
+                critical_cases = response['response'][x]['cases']['critical']
+                recovered_cases = response['response'][x]['cases']['recovered']
+                total_cases = response['response'][x]['cases']['total']
+                death_total = response['response'][x]['deaths']['total']
+                new_death = response['response'][x]['deaths']['new']
+                test= response['response'][x]['tests']['total']
+                time = response['response'][x]['time']
+                continent = response['response'][x]['continent']
+                print(continent)
+        context = { 'selectedcountry':selectedcountry, 
+                    'mylist': mylist,
+                    'population':population, 
+                    'new_cases':new_cases, 
+                    'active_cases':active_cases, 
+                    'critical_cases':critical_cases, 
+                    'recovered_cases':recovered_cases, 
+                    'total_cases':total_cases, 
+                    'death_total':death_total,
+                    'new_death':new_death,
+                    'test':test,
+                    'time':time,
+                    'continent':continent,
+                     }
         return render(request, 'home.html', context)
     
     content = {'mylist':mylist}
